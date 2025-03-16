39c0f87dc7e474e45ad31c369756e58541fb789f
johnnydang07 <khangvi07112003@gmail.com>
2025-03-16 15:34:18 +0700

    first commit

---
+++ array.js
@@ -0,0 +1,44 @@
+// Bai 1
+function caculateShoppingBudget(listNeedToBuy) {
+    let sum = 0;
+    for (i = 0; i < listNeedToBuy.length; i = i + 1) {
+        unit = listNeedToBuy[i][0];
+        price = listNeedToBuy[i][1];
+        sum += unit * price;
+    }
+    return sum;
+}
+listNeedToBuy = [[1, 20], [3, 40], [5, 15]];
+console.log(caculateShoppingBudget(listNeedToBuy));
+
+// Bai 2 
+list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+function snail(list) {
+    let result = [];
+    let left = 0;
+    let right = list[0].length;
+    let top = 0;
+    let bottom = list.length;
+
+    while(left < right || top < bottom){
+        for (let i = left; i < right; i++) {
+            result.push(list[left][i]);
+        }
+        top++;
+        for (let i = top; i < bottom; i++) {
+            result.push(list[i][right - 1]);
+        }
+        right--;
+        for (let i = right - 1; i >= left; i--) {
+            result.push(list[bottom - 1][i]);
+        }
+        bottom--;
+        for (let i = bottom - 1; i >= top; i--) {
+           result.push(list[i][left]);
+        }
+        left++;
+        console.log(bottom, top, left, right);
+    }
+    return result;
+}
+console.log(snail(list));
